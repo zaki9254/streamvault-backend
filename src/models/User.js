a009acee36e827b1a5de3b90f5b4a83671f97d95
zaki9254 <sharfzaki@gmail.com>
2026-04-22 19:52:37 +0530

    added: user schema

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, minlength: 6 },
+    avatar: { type: String, default: "" },
+    channelName: { type: String, trim: true },
+    bio: { type: String, default: "" },
+    subscribers: { type: Number, default: 0 },
+    googleId: { type: String, default: null },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password") || !this.password) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
